87f3bf90c95b8ec4ddc261d4aea91f64d22c8e7f
Kit Kennedy <kit@vinci.co>
2020-06-07 17:53:06 -0700

    Basic stuff on speech recognition

---
+++ swift/speechrecognition.swift
@@ -0,0 +1,4 @@
+// See https://medium.com/ios-os-x-development/speech-recognition-with-swift-in-ios-10-50d5f4e59c48 for an MVP
+
+// dealing with multiple results from https://developer.apple.com/documentation/speech/sfspeechrecognitionresult
+// https://stackoverflow.com/questions/58795543/speechkit-function-result-called-multiple-times/62253498#62253498
