cbcf9636c7fe538780817974836d9addf7732d6e
Kit Kennedy <kitkennedy8@gmail.com>
2020-11-15 17:12:52 -0800

    Adding a pylint file

---
+++ python/pylint.py
@@ -0,0 +1,9 @@
+# pylint: disable=arguments-differ
+
+# pylint: disable=multiple-statements
+# multiple-statements is disabled
+# pylint: enable=multiple-statements
+# now it's enabled
+
+def blah(): #pylint: disable=consider-using-enumerate
+    pass
