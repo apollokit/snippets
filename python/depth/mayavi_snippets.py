1d69fc71175174d0ad7497e7183e9586b46af7df
Kit Kennedy <kitkennedy8@gmail.com>
2020-11-16 16:56:44 -0800

    adding some snippets for 3d plotting in python

---
+++ python/depth/mayavi_snippets.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+
+from mayavi import mlab
+
+from vinci.cv.utils import remap_gray_image
+
+depth = np.load('doublestrike.npy')
+depth = remap_gray_image(depth)
+
+h, w = depth.shape
+y = np.arange(h)
+x = np.arange(w)
+
+x_m, y_m = np.meshgrid(x, y)
+
+mlab.clf()
+
+# see:
+# http://docs.enthought.com/mayavi/mayavi/auto/mlab_helper_functions.html?highlight=contour_surf
+# have to use transpose, because reasons... (it won't render correctly
+# otherwise)
+# mlab.surf(x_m.T, y_m.T, depth)
+
+# see: http://docs.enthought.com/mayavi/mayavi/auto/mlab_helper_functions.html?highlight=contour_surf#mesh
+mlab.mesh(x_m, -1*depth, -1*y_m)
+# mlab.show()
+
+# view the file with: $ ctmviewer shape.obj
+# note won't have color map
+mlab.savefig("shape.obj")
