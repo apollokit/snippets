fdd8648490b3bdaae6ab01a99e3d291835409b62
Kit Kennedy <kitkennedy8@gmail.com>
2023-08-15 11:47:01 -0700

    print cpp

--- cpp/print_cpp.cpp
+++ cpp/print_cpp.cpp
@@ -19,8 +19,9 @@ static void test(uint64_t simTimeNanos, Eigen::Vector3d r_I)
     // for uint8_t, unint16_t
     // https://stackoverflow.com/a/50882761/4292910
     printf("incomingCmdMode: %hu\n", incomingCmdMode);
-    // // for unint16_t (FFS cpp...)
+    // for unint16_t
     // printf("incomingFiringDuration: %hu\n", incomingFiringDuration);
+    printf("incomingFiringDuration: %u\n", (unsigned int)incomingFiringDuration);
     // for float
     printf("GravBodyData::computeGravityInertial: ri [%.15f, %.15f, %.15f]\n", r_I[0], r_I[1], r_I[2]);
 }
